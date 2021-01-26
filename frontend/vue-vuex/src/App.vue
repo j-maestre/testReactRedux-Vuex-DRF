9711b739e439a23511567849a20580093c1ca364
tonomolla6 <tonomolla6@gmail.com>
2021-01-26 19:59:30 +0100

    Tono -> Init project VUE, add router-link component

---
+++ frontend/vue-vuex/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+  </div>
+  <router-view />
+</template>
+
+<script>
+
+export default {
+  name: "App",
+  components: {
+  },
+};
+</script>
+
+<style>
+#nav {
+  
+}
+</style>
