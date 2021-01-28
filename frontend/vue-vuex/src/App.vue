22b298e921a4c146f560e7eab13799aed2156e20
tonomolla6 <tonomolla6@gmail.com>
2021-01-28 20:46:08 +0100

    Tono -> Docker vue perfecto

--- frontend/vue-vuex/src/App.vue
+++ frontend/vue-vuex/src/App.vue
@@ -2,7 +2,7 @@
   <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-      <router-link to="/travels">Travels</router-link>
+      <router-link to="/travels">Travelwwws</router-link>
   </div>
 </template>
 
