aa678383ce80180924145a1a3c1d284b74a817d3
tonomolla6 <tonomolla6@gmail.com>
2021-01-28 19:21:24 +0100

    Tono -> Dockerize vue

--- frontend/vue-vuex/src/App.vue
+++ frontend/vue-vuex/src/App.vue
@@ -1,17 +1,8 @@
 <template>
-<<<<<<< HEAD
-  <div class="app">
-    <div id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-=======
   <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
       <router-link to="/travels">Travels</router-link>
->>>>>>> 5373215111d240f0a2f33759b9f5731434738fa7
   </div>
 </template>
 
