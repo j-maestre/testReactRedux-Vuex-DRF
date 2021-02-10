af71120bf5086a5d4e715c864aa7245047c4971e
tonomolla6 <tonomollag6@gmail.com>
2021-02-10 21:31:40 +0100

    Tono -> Pruebas inutiles pero que alomejor sirven

--- frontend/vue-vuex/src/api/services/user.service.js
+++ frontend/vue-vuex/src/api/services/user.service.js
@@ -4,9 +4,11 @@ export function login() {
   return axios.get("https://api.coindesk.com/v1/bpi/currentprice.json");
 }
 
-export function register(credentials) {
-  const user = { user: credentials };
-  return axios
-    .post("http://127.0.0.1:8000/api/users/", user)
-    .then((response) => response);
+export function register() {
+  // const user = { user: credentials };
+  let http = axios.create({
+    baseURL: "http://localhost:8000/api",
+  });
+
+  return http.get("/travels");
 }
